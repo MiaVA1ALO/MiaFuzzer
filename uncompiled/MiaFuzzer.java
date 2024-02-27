73341824578fe84ae5035817ce4f7814589592d4
MiaVA1ALO <114697309+MiaVA1ALO@users.noreply.github.com>
2024-02-27 12:16:08 -0400

    Update MiaFuzzer.java
    new version because the code is old

--- uncompiled/MiaFuzzer.java
+++ uncompiled/MiaFuzzer.java
@@ -6,64 +6,59 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-// Sends requests to a website to test the response it gives.
-// Example: www.google.ca would be a 200 response but www.google.ca/404 would be a 404.
-// Useful for bruteforcing website directories.
-
-/**
- * README.txt
- *
- * Usage: MiaFuzzer www.url.com [PAYLOAD].txt blockedresponse,blockedresponse,[...]
- * www.url.com is the website you wish to fuzz. The url should have FUZZ somewhere within.
- * Example: www.url.com/FUZZ, or www.url.com/link.FUZZ, etc
- *
- * [PAYLOAD].txt is the directory of the list of payloads, each separated by a new line.
- * Example: If your URL is www.url.com/FUZZ and your .txt file says:
- * index
- * test
- * 123
- * the URLs www.url.com/index, www.url.com/test, and www.url.com/123 will be tested.
- * The location is relative to the .class file.
- *
- * blockedresponse,blockedresponse,[...] are the different HTTP responses you want to ignore.
- * Ones commonly useless are 404, 403, etc.
- * This argument is passed as 404,403,402 and you can add as many as needed.
- *
- * Example arguments:
- * java MiaFuzzer www.url.com/FUZZ.php special_payloads.txt 404,403,302,301
- */
 public class MiaFuzzer
 {
-    public static final String logo = "#     # ###    #    #######\n##   ##  #    # #   #       #    # ###### ###### ###### #####\n# # # #  #   #   #  #       #    #     #      #  #      #    #\n#  #  #  #  #     # #####   #    #    #      #   #####  #    #\n#     #  #  ####### #       #    #   #      #    #      #####\n#     #  #  #     # #       #    #  #      #     #      #   #\n#     # ### #     # #        ####  ###### ###### ###### #    #\n";
+    // "MiaFuzzer" in ASCII art
+    private static final String logo = "#     # ###    #    #######\n##   ##  #    # #   #       #    # ###### ###### ###### #####\n# # # #  #   #   #  #       #    #     #      #  #      #    #\n#  #  #  #  #     # #####   #    #    #      #   #####  #    #\n#     #  #  ####### #       #    #   #      #    #      #####\n#     #  #  #     # #       #    #  #      #     #      #   #\n#     # ### #     # #        ####  ###### ###### ###### #    #\n";
     public static void main(String[] args)
     {
-        String URL;
-        if(args.length != 3)
+        String url;
+        if(args.length < 1)
         {
-            System.out.println("Usage: java MiaFuzzer www.url.com [PAYLOAD].txt blockedresponse,blockedresponse,[...]");
+            System.out.println("Usage: java MiaFuzzer www.url.com [PAYLOAD].txt blockedresponse,blockedresponse,[...]\nor\nUsage: java MiaFuzzer www.url.com [PAYLOAD].txt");
         }
         else
         {
-            URL = args[0];
-            System.out.println(logo);
-            // prevents an error from the URL being passed as www.example.com without an http
-            if(!URL.contains("http"))
+            // for if there are no blocked response arguments
+            String temp;
+            int[] blockedResponses;
+
+            if(args.length == 2)
             {
-                URL = "http://" + URL;
+                temp = "-1";
+            }
+            else
+            {
+                temp = args[2];
+            }
+            blockedResponses = separateArgs(temp);
+            url = args[0];
+
+            System.out.println(logo);
+
+            // prevents an error from the URL being passed as www.example.com without an http
+            if(!url.contains("http://"))
+            {
+                url = "http://" + url;
+            }
+            // this script uses http, however it probably works without this if statement.
+            if(url.contains("https://"))
+            {
+                url = url.replace("https://", "http://");
             }
             // for the script to function properly the URL needs to contain 'FUZZ'
-            if(!URL.contains("FUZZ"))
+            if(!url.contains("FUZZ"))
             {
-                URL = URL + "FUZZ";
-                System.err.println("URL does not contain 'FUZZ'. URL automatically set to " + URL);
+                url += "FUZZ";
+                System.err.println("URL does not contain 'FUZZ'. URL automatically set to " + url);
             }
-            new MiaFuzzer(URL, args[1], separateResponseArg(args[2]));
-            System.out.println(Arrays.toString(separateResponseArg(args[2])));
+            new MiaFuzzer(url, args[1], blockedResponses);
+            System.out.println(Arrays.toString(separateArgs(args[2])));
         }
     }
 
-    // initializer method, controls all the major functions
-    public MiaFuzzer(String URL, String filePath, int[] blockedResponses)
+    // left public in case someone wants to write something better than my main() (hint hint...)
+    public MiaFuzzer(String url, String file, int[] blockedResponses)
     {
         int totalRequests = 0;
         int blockedRequests = 0;
@@ -72,17 +67,17 @@ public class MiaFuzzer
         try
         {
             // opens [PAYLOAD].txt
-            payloads = readPayloads(filePath);
+            payloads = readPayload(file);
         }
         catch (IOException e)
         {
-            // not specifically a file not found error but I think that's the only possible one
-            System.err.println("File not found: " + filePath);
+            // not specifically a file not found error, but I think that's the only possible one
+            System.err.println("File not found: " + file);
         }
         for (String payload : payloads)
         {
-            // replaces 'FUZZ' with something else - i.e. /index.html or something
-            String temp = URL.replace("FUZZ", payload);
+            // replaces 'FUZZ' with the next payload from the file.
+            String temp = url.replace("FUZZ", payload);
             int response = getStatus(temp);
             if (!isBlocked(response, blockedResponses))
             {
@@ -104,17 +99,18 @@ public class MiaFuzzer
             }
         }
         System.out.println("\nTotal requests: " + totalRequests + "\nAllowed requests: " + allowedRequests);
-        System.err.println("Blocked requests: " + blockedRequests);
+        System.err.println("Blocked requests: " + blockedRequests); // err.println is red text (not in cmd)
     }
 
-    int getStatus(String URL)
+    // gets a status (404, 300, 200 whatever)
+    int getStatus(String url)
     {
-        int statusCode;
+        int status;
         try
         {
-            URL testURL = new URL(URL);
+            URL testURL = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) testURL.openConnection();
-            statusCode = connection.getResponseCode();
+            status = connection.getResponseCode();
         }
         catch (IOException e)
         {
@@ -122,24 +118,24 @@ public class MiaFuzzer
             // where it would just be 404 anyway.
             return 404;
         }
-        return statusCode;
+        return status;
     }
 
     // reads the payloads from [PAYLOAD].txt and converts to a String[]
-    String[] readPayloads(String filePath) throws IOException
+    String[] readPayload(String file) throws IOException
     {
-        ArrayList<String> payloadList = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+        try (BufferedReader bReader = new BufferedReader(new FileReader(file)))
         {
             String line;
-            while ((line = br.readLine()) != null)
+            while ((line = bReader.readLine()) != null)
             {
-                payloadList.add(line);
+                list.add(line);
             }
         }
 
-        return payloadList.toArray(new String[0]);
+        return list.toArray(new String[0]);
     }
 
     // tests if the request is in the user's list of blocked responses (i.e. 404)
@@ -156,13 +152,13 @@ public class MiaFuzzer
     }
 
     // separates args[2] into an int[] (i.e. 404,403,302 = [404, 403, 302])
-    static int[] separateResponseArg(String arg)
+    static int[] separateArgs(String arg)
     {
-        String[] splitString = arg.split(",");
-        int[] responses = new int[splitString.length];
-        for (int i = 0; i < splitString.length; i++)
+        String[] string = arg.split(",");
+        int[] responses = new int[string.length];
+        for (int i = 0; i < string.length; i++)
         {
-            responses[i] = Integer.parseInt(splitString[i].trim());
+            responses[i] = Integer.parseInt(string[i].trim());
         }
         return responses;
     }
